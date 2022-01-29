ac4406f6a5de5087a08e19dc32a53a0e7f8c5bd4
Bchernovol <boryacernovol@gmail.com>
2022-01-29 20:01:27 +0200

    Form for adding articles

--- src/main/java/com/bchernovol/blog1/models/Post.java
+++ src/main/java/com/bchernovol/blog1/models/Post.java
@@ -55,4 +55,13 @@ public class Post {
     public void setVievs(int vievs) {
         this.vievs = vievs;
     }
+    public Post(){
+
+    }
+    public Post(String title,String anons, String full_text){
+        this.title=title;
+        this.anons=anons;
+        this.full_text=full_text;
+    }
+
 }
